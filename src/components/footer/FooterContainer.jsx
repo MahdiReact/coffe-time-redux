5e4e97532e6967632db672326e1049721fc2b56a
MahdiKoohnehsin <MahdiKoohnehsin81@gmail.com>
2022-06-30 17:02:04 +0430

    continue of footer

--- src/components/footer/FooterContainer.jsx
+++ src/components/footer/FooterContainer.jsx
@@ -8,11 +8,30 @@ const FooterContainer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-container-border">
-        <div>
-          <FooterNav/>
+        <div className="footer-nav-container">
+          <FooterNav />
+        </div>
+        <div className="footer-1-2-row">
+          <div className="footer-grid-container">
+            <div className="footer-grid-item">
+              <img src="https://www.digikala.com/statics/img/png/enamad.png" />
+            </div>
+            <div className="footer-grid-item">
+              <img src="https://www.digikala.com/statics/img/png/kasbokar.png" />
+            </div>
+            <div className="footer-grid-item">
+              <img src="https://www.digikala.com/statics/img/png/rezi.png" />
+            </div>
+          </div>
+          <div className="footer-cafe-desc">
+            <h3>درباره کافی تایم...</h3>
+            <p>خوش بختیم که ثابت کرده ایم بهترین متریال  قهوه با بهترین رست و عالی ترین درجه کیفیت را به شما ارایه میدهیم.
+              اعتبار ما  کیفیت ماست.
+            </p>
+          </div>
         </div>
       </div>
-     <Author/>
+      <Author />
     </footer>
   );
 };
